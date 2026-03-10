7084d942a80d2c9176e9d0f5532093184e379464
lio <s32240178@student.ubm.ac.id>
2026-03-10 12:17:13 +0700

    add login css and js

--- auth/login.php
+++ auth/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lost and Found</title>
-    <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="../assets/login.css" />
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -42,5 +42,6 @@
             });
         });
     </script>
+    <script type="module" src="assets/login.js"></script>
 </body>
 </html>
